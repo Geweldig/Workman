689dbb338c5d40710352b77d7335cc0ba283cf09
Bram Crielaard <bram@crielaard.co.uk>
2017-07-21 13:45:18 +0200

    Update install.sh
    Copy the version and help files during install

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ if [ ! -d ~/.workspaces ]; then
 	mkdir ~/.workspaces
 fi
 cp workspace.sh ~/.workspaces
+cp version ~/.workspaces
+cp help ~/.workspaces
 if [ -f ~/.bashrc ]; then
 	echo "source ~/.workspaces/workspace.sh" >> ~/.bashrc
 fi
