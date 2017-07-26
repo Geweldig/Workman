ce0363d6ca5acd09d1aa3f28474f5d165df65f8d
Bram Crielaard <bram@crielaard.co.uk>
2017-07-26 16:22:03 +0200

    Fix exitcodes
    Exitcodes now work correctly

--- workspace.sh
+++ workspace.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 function workspace() {
 	local DIR=~/.workspaces
+	local EXITCODE=0
 
 	if [ ! -d $DIR ]; then
 		mkdir $DIR
@@ -12,29 +13,36 @@ function workspace() {
 			select yn in "Yes" "No"; do
 			    case $yn in
 			        Yes ) break;;
-			        No ) return 0;;
+			        No ) return $EXITCODE;;
 			    esac
 			done
 		fi
 		pwd > $DIR/.$1
+		EXITCODE=0
 	}
 
 	__workspaceman_delete() {
 		if [ -f $DIR/.$1 ]; then
 			rm $DIR/.$1
+			EXITCODE=0
+		else
+			EXITCODE=1
 		fi
 	}
 
 	__workspaceman_list() {
 		ls -A $DIR | grep ^\\. | sed s/\.//
+		EXITCODE=0
 	}
 
 	__workspaceman_help() {
 		cat "$DIR/help"
+		EXITCODE=0
 	}
 
 	__workspaceman_version() {
 		cat "$DIR/version"
+		EXITCODE=0
 	}
 	
 	# Handle flags, getops breaks for functions
@@ -47,24 +55,25 @@ function workspace() {
 				__workspaceman_add $2
 			else
 				echo "A name is required when adding a new workspace."
-				return 1
+				EXITCODE=1
+				return $EXITCODE
 			fi
-			return 0
+			return $EXITCODE
 
 		# Handle -l/--list flag
 		elif [ "$1" = "-l" ] || [ "$1" = "--list" ]; then
 			__workspaceman_list
-			return 0
+			return $EXITCODE
 
 		# Handle -h/--help flag
 		elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 			__workspaceman_help
-			return 0
+			return $EXITCODE
 
 		# Handle -v/--version flag
 		elif [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
 			__workspaceman_version
-			return 0
+			return $EXITCODE
 
 		# Handle -d/--delete flag
 		elif [ "$1" = "-d" ] || [ "$1" = "--delete" ]; then
@@ -73,23 +82,26 @@ function workspace() {
 				__workspaceman_delete $2
 			else
 				echo "A name is required when deleting a workspace."
-				return 1
+				EXITCODE=1
+				return $EXITCODE
 			fi
-			return 0
+			return $EXITCODE
 		fi
 
 	# Handle all other commands (i.e, switching directory)
 	elif [ ! -z "$1" ] && [ -f $DIR/.$1 ]; then
 		cd $(head -n 1 $DIR/.$1)
 		cp $DIR/.$1 $DIR/last_used 2> /dev/null
-		return 0
+		return $EXITCODE
 	elif [ ! -z "$1" ] && [ ! -f $DIR/.$1 ]; then
 		echo "workspace not found: $1"
-		return 1
+		EXITCODE=1
+		return $EXITCODE
 	elif [ -f $DIR/last_used ]; then
 		cd $(head -n 1 $DIR/last_used)
-		return 0
+		return $EXITCODE
 	fi
 	
-	return 1
+	EXITCODE=1
+	return $EXITCODE
 }
