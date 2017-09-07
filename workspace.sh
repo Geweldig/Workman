506c36930b2ce2320b5aa6ad745b461e84432787
Bram Crielaard <bram@crielaard.co.uk>
2017-09-07 22:11:58 +0200

    Make ifs safe

--- workspace.sh
+++ workspace.sh
@@ -3,12 +3,12 @@ function workspace() {
 	local DIR=~/.workspaces
 	local EXITCODE=0
 
-	if [ ! -d $DIR ]; then
+	if [[ ! -d $DIR ]]; then
 		mkdir $DIR
 	fi
 
 	__workspaceman_add() {
-		if [ -f $DIR/.$1 ]; then
+		if [[ -f $DIR/.$1 ]]; then
 			echo "Workspace with this name already exists, overwrite? "
 			select yn in "Yes" "No"; do
 			    case $yn in
@@ -22,7 +22,7 @@ function workspace() {
 	}
 
 	__workspaceman_delete() {
-		if [ -f $DIR/.$1 ]; then
+		if [[ -f $DIR/.$1 ]]; then
 			rm $DIR/.$1
 			EXITCODE=0
 		else
@@ -46,12 +46,12 @@ function workspace() {
 	}
 	
 	# Handle flags, getops breaks for functions
-	if [ ! -z "$1" ] && [[ $1 =~ ^- ]]; then
+	if [[ ! -z "$1" ]] && [[ $1 =~ ^- ]]; then
 		
 		# Handle -a/--add flag
-		if [ "$1" = "-a" ] || [ "$1" = "--add" ]; then
+		if [[ "$1" = "-a" ]] || [[ "$1" = "--add" ]]; then
 			# Second parameter is required
-			if [ ! -z "$2" ]; then
+			if [[ ! -z "$2" ]]; then
 				__workspaceman_add $2
 			else
 				echo "A name is required when adding a new workspace."
@@ -61,24 +61,24 @@ function workspace() {
 			return $EXITCODE
 
 		# Handle -l/--list flag
-		elif [ "$1" = "-l" ] || [ "$1" = "--list" ]; then
+		elif [[ "$1" = "-l" ]] || [[ "$1" = "--list" ]]; then
 			__workspaceman_list
 			return $EXITCODE
 
 		# Handle -h/--help flag
-		elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+		elif [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]]; then
 			__workspaceman_help
 			return $EXITCODE
 
 		# Handle -v/--version flag
-		elif [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
+		elif [[ "$1" = "-v" ]] || [[ "$1" = "--version" ]]; then
 			__workspaceman_version
 			return $EXITCODE
 
 		# Handle -d/--delete flag
-		elif [ "$1" = "-d" ] || [ "$1" = "--delete" ]; then
+		elif [[ "$1" = "-d" ]] || [[ "$1" = "--delete" ]]; then
 			# Second parameter is required
-			if [ ! -z "$2" ]; then
+			if [[ ! -z "$2" ]]; then
 				__workspaceman_delete $2
 			else
 				echo "A name is required when deleting a workspace."
@@ -89,15 +89,15 @@ function workspace() {
 		fi
 
 	# Handle all other commands (i.e, switching directory)
-	elif [ ! -z "$1" ] && [ -f $DIR/.$1 ]; then
+	elif [[ ! -z "$1" ]] && [[ -f $DIR/.$1 ]]; then
 		cd $(head -n 1 $DIR/.$1)
 		cp $DIR/.$1 $DIR/last_used 2> /dev/null
 		return $EXITCODE
-	elif [ ! -z "$1" ] && [ ! -f $DIR/.$1 ]; then
+	elif [[ ! -z "$1" ]] && [[ ! -f $DIR/.$1 ]]; then
 		echo "workspace not found: $1"
 		EXITCODE=1
 		return $EXITCODE
-	elif [ -f $DIR/last_used ]; then
+	elif [[ -f $DIR/last_used ]]; then
 		cd $(head -n 1 $DIR/last_used)
 		return $EXITCODE
 	fi
